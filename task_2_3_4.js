1d59918e72c4dddbc6a3da1b711e64385020f50d
Xemur0 <77635277+Xemur0@users.noreply.github.com>
2021-08-13 16:42:26 +0300

    les_1

---
+++ task_2_3_4.js
@@ -0,0 +1,18 @@
+{
+    let admin;
+    let name;
+    name = 'Василий';
+    console.log(admin);
+    admin = name;
+    console.log(admin)
+}
+
+{
+    console.log(1000 + '108')
+}
+
+/*
+Судя по тому что вычитал:
+defer - сохраняет относительный порядок загрузки скриптов
+async - что первое загрузилось, то и отобразится
+ */
